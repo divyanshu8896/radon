e45c1c355bc58c5013e1a67ab73aaa96a0e762f6
divyanshu <yo98u@gmaile.com>
2022-06-05 04:00:34 +0530

    module js

---
+++ src/routes/logger1.js
@@ -0,0 +1,10 @@
+const endpoint = "https://www.functionup.org"
+const cohort ="radon"
+
+const log = function(){
+    console.log('I am inside the log function')
+}
+
+module.exports.url=endpoint
+module.exports.cohort=cohort
+module.exports.log=log
